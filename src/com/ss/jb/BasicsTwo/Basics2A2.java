1bcd22c6bf0330566c14c1ed51e2fa16c29710ed
128968 <07161668>
2021-09-14 15:44:31 -0400

    other assignment stubbing

---
+++ src/com/ss/jb/BasicsTwo/Basics2A2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.jb.BasicsTwo;
+
+/**
+ * @author brandon
+ *
+ */
+public class Basics2A2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/ss/jb/BasicsTwo/Basics2A3.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.ss.jb.BasicsTwo;
+
+/**
+ * @author brandon
+ *
+ */
+public class Basics2A3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	abstract class Shape
+	{
+		public abstract float calculateArea() ;
+	}
+
+}
