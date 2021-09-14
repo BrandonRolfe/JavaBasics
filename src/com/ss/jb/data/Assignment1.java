262fb93afb95fd924a0c19a90b646d67f54acf2b
Brandon Rolfe <BrandonRolf@Smoothstack.com>
2021-09-14 11:25:12 -0400

    initial commit

---
+++ src/com/ss/jb/data/Assignment1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.jb.data;
+
+/**
+ * @author brandon
+ *
+ */
+public class Assignment1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author brandon
+ *
+ */
+module JavaBasics {
+}
