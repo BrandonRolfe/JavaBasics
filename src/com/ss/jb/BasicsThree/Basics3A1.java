025edb2208965861993be5b61031a2b3c95b49f9
128968 <07161668>
2021-09-15 17:14:01 -0400

    basics 3 directory

---
+++ src/com/ss/jb/BasicsThree/Basics3A1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.jb.BasicsThree;
+
+/**
+ * @author brandon
+ *
+ */
+public class Basics3A1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
